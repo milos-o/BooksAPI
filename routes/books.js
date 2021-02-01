72e61dbdc94c7c58a9f5f3be63502d05cd8e0145
Miloš Ostojić <61186768+milos-o@users.noreply.github.com>
2021-02-01 14:55:39 +0100

    fix

--- routes/books.js
+++ routes/books.js
@@ -9,10 +9,10 @@ const router = express.Router();
 
 router.get('/books', isAuth,  BookController.findAllBooks);
 
-router.get('/books/:id',  isAuth,  BookController.findBookById);
+router.get('/books/:id',  isAuth, isAdmin,  BookController.findBookById);
 
 
-router.get('/book_num/:id',  isAuth,  BookController.numberOfBooks);
+router.get('/book_num/:id',  isAuth, isAdmin,  BookController.numberOfBooks);
 
 router.post('/add-new', [
     body('price')
@@ -31,15 +31,15 @@ router.post('/add-new', [
       body('pages')
       .isNumeric({ min: 10, max: 50000 })
       
-  ],  isAuth, BookController.addNew);
+  ],  isAuth, isAdmin, BookController.addNew);
 
-router.post('/edit-book',  isAuth,  BookController.postEditProduct);
+router.post('/edit-book',  isAuth, isAdmin,  BookController.postEditProduct);
 
 router.post('/book_inc/:id', isAuth,  BookController.AddOneBook);
 
 router.post('/book_dec/:id', isAuth,  BookController.RemoveOneBook);
 
-router.delete('/delete-book', isAuth, BookController.postDeleteProduct);
+router.delete('/delete-book', isAuth, isAdmin, BookController.postDeleteProduct);
 
 router.get('/values',  isAuth, BookController.getMoney);
 
